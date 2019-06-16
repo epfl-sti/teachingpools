52cfa4666c09ef45e504619622420438eb091ca0
Emmanuel Jaep <emmanuel.jaep@epfl.ch>
2019-06-16 12:16:05 +0200

    added bootstrap4 and fontawesome. added simple templates and forms for POC

--- teaching_pool_project/web/views.py
+++ teaching_pool_project/web/views.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+
+from .forms import NameForm
+
+def index(request):
+    if request.method== 'POST':
+        form = NameForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/')
+    else:
+        form = NameForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'web/index.html', context)
