e2f943398de1dde09328a9bf1eaf889977ab9892
Emmanuel Jaep <emmanuel.jaep@epfl.ch>
2019-06-20 13:50:07 +0200

    clean up headers added logic in case the given object did not exist.

--- teaching_pool_project/api/views.py
+++ teaching_pool_project/api/views.py
@@ -1,13 +1,11 @@
-# from django.shortcuts import render
-
-# Create your views here.
-
 from rest_framework import generics, permissions
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from django.http import Http404
 
 from .serializers import PersonSerializer, CourseSerializer, TeachingSerializer
 from web.models import Person, Course, Teaching
@@ -37,8 +35,11 @@ class CourseDetailByYearTermCode(generics.RetrieveUpdateDestroyAPIView):
         year = self.kwargs.get('year')
         term = self.kwargs.get('term')
         code = self.kwargs.get('code')
+        try:
         course = self.queryset.get(year=year, term=term, code=code)
         return course
+        except:
+            raise Http404
 
 
 @authentication_classes((SessionAuthentication, BasicAuthentication))
