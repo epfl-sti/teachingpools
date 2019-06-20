c1ed47a0c84dd7194f3fa0babf2f589af367ce83
Emmanuel Jaep <emmanuel.jaep@epfl.ch>
2019-06-20 13:52:08 +0200

    fixed patterns not working when retrieving collections of objects

--- teaching_pool_project/api/urls.py
+++ teaching_pool_project/api/urls.py
@@ -5,11 +5,11 @@ from .views import *
 app_name = 'api'
 
 urlpatterns = {
-    url(r'v1/persons/$', Persons.as_view(), name="persons"),
-    url(r'v1/persons/?(?P<pk>[^/]+)/$', PersonDetail.as_view(), name="person_details"),
-    url(r'v1/courses/$', Courses.as_view(), name="courses"),
-    url(r'v1/courses/(?P<pk>\d*)/$', CourseDetail.as_view(), name="course_details_by_id"),
-    url(r'v1/courses/(?P<year>.*)/(?P<term>.*)/(?P<code>.*)/$', CourseDetailByYearTermCode.as_view(), name="course_details_by_year_term_code"),
+    url(r'v1/persons/', Persons.as_view(), name="persons"),
+    url(r'v1/person/?(?P<pk>[^/]+)/$', PersonDetail.as_view(), name="person_details"),
+    url(r'v1/courses/', Courses.as_view(), name="courses"),
+    url(r'v1/course/(?P<pk>\d*)/$', CourseDetail.as_view(), name="course_details_by_id"),
+    url(r'v1/course/(?P<year>.*)/(?P<term>.*)/(?P<code>.*)/$', CourseDetailByYearTermCode.as_view(), name="course_details_by_year_term_code"),
     url(r'v1/teachings/$', Teachings.as_view(), name="teachings"),
     url(r'v1/teachings/(?P<sciper>\d*)/$', TeachingsBySciper.as_view(), name="teachings_by_sciper")
 }
