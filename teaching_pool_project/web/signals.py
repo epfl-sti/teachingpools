a991aef0861421c7209bf907dcfa863a536511ee
Emmanuel Jaep <emmanuel.jaep@epfl.ch>
2019-07-01 10:34:10 +0200

    removed signal (because we are not using ldap anymore) added logging when user logs in

--- teaching_pool_project/web/signals.py
+++ teaching_pool_project/web/signals.py
@@ -1,19 +1,12 @@
+import logging
+
 from django.conf import settings
-from django.contrib.auth.models import Group
 from django.contrib.auth.signals import user_logged_in
 from django.dispatch import receiver
 
-from epfl.sti.helpers import ldap as epfl_ldap
-from web.models import Person
+logger = logging.getLogger(__name__)
 
 
 @receiver(user_logged_in)
-def check_if_user_is_phd(sender, request, **kwargs):
-    if epfl_ldap.is_phd(settings, request.user.sciper):
-        if not request.user.groups.filter(name="phds").exists():
-            phd_group = Group.objects.get(name="phds")
-            phd_group.user_set.add(request.user)
-    else:
-        if request.user.groups.filter(name="phds").exists():
-            phd_group = Group.objects.get(name="phds")
-            phd_group.user_set.remove(request.user)
+def log_user_logged_in(sender, request, **kwargs):
+    logger.info("User %s logged in", request.user.username)
