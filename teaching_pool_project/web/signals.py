02f2c51b071787895c8843eefcf444e3db77b43c
Emmanuel Jaep <emmanuel.jaep@epfl.ch>
2019-07-01 10:16:58 +0200

    formatting

--- teaching_pool_project/web/signals.py
+++ teaching_pool_project/web/signals.py
@@ -1,9 +1,11 @@
 from django.conf import settings
+from django.contrib.auth.models import Group
 from django.contrib.auth.signals import user_logged_in
 from django.dispatch import receiver
-from django.contrib.auth.models import Group
+
 from epfl.sti.helpers import ldap as epfl_ldap
 from web.models import Person
+
 
 @receiver(user_logged_in)
 def check_if_user_is_phd(sender, request, **kwargs):
