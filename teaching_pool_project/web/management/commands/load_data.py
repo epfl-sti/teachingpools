655bfff6d9d64e21088f8ad51f1a75f021399728
Emmanuel Jaep <emmanuel.jaep@epfl.ch>
2019-07-03 15:00:28 +0200

    reviewed data loader to make sure the number of students are correctly loaded. added logging to data loader to keep a trace

--- teaching_pool_project/web/management/commands/load_data.py
+++ teaching_pool_project/web/management/commands/load_data.py
@@ -1,7 +1,8 @@
 import io
-from math import ceil
+import logging
 import os.path
 import pickle
+from math import ceil
 
 import numpy
 import pandas as pd
@@ -13,13 +14,22 @@ from django.core.management.base import BaseCommand
 from epfl.sti.helpers import ldap as epfl_ldap
 from web.models import Course, Person, Teaching
 
+logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     def handle(self, **options):
+        logger.info("loading courses for the current year")
+
+        # Sanity check to make sure that the required groups are present
+        check_groups_are_present()
+
         # Load the courses for the current year
         df = pd.read_excel(settings.EXCEL_FILE_TO_LOAD)
         for index, row in df.iterrows():
-            course_code = row['code']
+            if type(row['code']) != type(str()):
+                course_code = ''
+            else:
+                course_code = row['code']
             subject = row['subject']
             if type(row['teachers']) != type(float()):
                 raw_teachers = [x.strip() for x in row['teachers'].split(';')]
@@ -27,7 +37,7 @@ class Command(BaseCommand):
                 raw_teachers = list()
             term = row['term']
             if type(row['type']) != type(float()):
-                raw_types = [x.strip() for x in row['type'].split(',')]
+                raw_types = [x.lower().strip() for x in row['type'].split(',')]
             else:
                 raw_types = list()
 
@@ -36,42 +46,76 @@ class Command(BaseCommand):
             students = int(row['students'])
 
             if type(row['languages']) != type(float()):
-                raw_languages = [x.strip()
+                raw_languages = [x.lower().strip()
                                  for x in row['languages'].split('/')]
             else:
                 raw_languages = list()
 
             teachers = load_teachers(raw_teachers)
-            load_course(settings.EXCEL_LOADER_CURRENT_YEAR, term, course_code,
-                        subject, raw_types, teachers, students, raw_languages)
+
+            if settings.EXCEL_LOADER_CURRENT_YEAR and term and course_code:
+                load_course(settings.EXCEL_LOADER_CURRENT_YEAR, term, course_code,
+                            subject, raw_types, teachers, students, raw_languages)
 
         # Now time to load the stats for the previous year
+        logger.info("Loading statistics from the previous year")
         df = pd.read_excel(settings.EXCEL_FILE_TO_LOAD_FOR_PREVIOUS_YEAR)
         for index, row in df.iterrows():
-            course_code = row['code']
-            term = row['term']
+
+            if type(row['code']) != type(str()):
+                course_code = ''
+            else:
+                course_code = row['code']
+
+            if type(row['term']) != type(str()):
+                term = ''
+            else:
+                term = row['term']
+
             if numpy.isnan(row['students']):
                 row['students'] = numpy.nan_to_num(row['students'])
             students = int(row['students'])
-            update_course(settings.EXCEL_LOADER_CURRENT_YEAR,
-                          term, course_code, students)
+
+            if settings.EXCEL_LOADER_CURRENT_YEAR and term and course_code:
+                update_course(settings.EXCEL_LOADER_CURRENT_YEAR,
+                            term, course_code, students)
+            else:
+                logger.warning("Unable to update course because one of the mandatory field was not provided")
+                logger.debug("year: %s", settings.EXCEL_LOADER_CURRENT_YEAR)
+                logger.debug("term: %s", term)
+                logger.debug("code: %s", course_code)
+                logger.debug("students: %s", students)
 
 
 def update_course(year, term, code, students):
+    logger.info("Updating number of students")
+    logger.debug("year: %s", year)
+    logger.debug("term: %s", term)
+    logger.debug("code: %s", code)
+    logger.debug("students: %s", students)
+
+    if not year or not term or not code or type(students)!=type(int()):
+        error_message = "A least one of the fields did not have a correct value"
+        logger.error(error_message)
+        raise ValueError("A least one of the fields did not have a correct value")
+
     try:
-        course = Course.objects.get(year=year, term=term, code=code)
+        course = Course.objects.get(year__iexact=year, term__iexact=term, code__iexact=code)
         course.numberOfStudents = students
         if course.has_exercises or course.has_practical_work:
             course.calculatedNumberOfTAs = int(ceil((students/25)))
+        else:
+            course.calculatedNumberOfTAs = 0
+
         course.save()
     except ObjectDoesNotExist:
-        print("Course not found in '{year}' -> term:{term}, code:{code}".format(
-            year=year, term=term, code=code))
+        logger.warning("Unable to find course")
 
 
 def load_teachers(teachers=list()):
-    # Sanity check to make sure that the required groups are present
-    check_groups_are_present()
+    logger.info("loading teachers")
+    logger.debug("teachers: %s", teachers)
+
     teachers_group = Group.objects.get(name="teachers")
 
     return_value = list()
@@ -112,44 +156,55 @@ def load_teachers(teachers=list()):
 
 
 def load_course(year='', term='', code='', subject='', types=list(), teachers=list(), students=int(), languages=list()):
-    try:
-        course = Course.objects.get(year=year, term=term, code=code)
-    except ObjectDoesNotExist:
-        course = Course()
-        course.year = year
-        course.term = term
-        course.code = code
-        course.subject = subject
-        course.numberOfStudents = students
-        if 'C' in types:
-            course.has_course = True
-        if 'Ex' in types:
-            course.has_exercises = True
-        if 'Proj' in types:
-            course.has_project = True
-        if 'TP' in types:
-            course.has_practical_work = True
+    logger.info("loading course")
+    logger.debug("year: %s", year)
+    logger.debug("term: %s", term)
+    logger.debug("code: %s", code)
+    logger.debug("subject: %s", subject)
+    logger.debug("types: %s", types)
+    logger.debug("teachers: %s", teachers)
+    logger.debug("students: %s", students)
+    logger.debug("languages: %s", languages)
 
-        if 'allemand' in languages:
-            course.taughtInGerman = True
-        if 'anglais' in languages:
-            course.taughtInEnglish = True
-        if 'français' in languages:
-            course.taughtInFrench = True
-        course.save()
+
+    # We don't want to manage courses that fill the minimum requirements
+    if year == '' or term == '' or code == '':
+        error_message = "At least one mandatory field (year, term, code) was not passed."
+        logger.error(error_message)
+        raise ValueError(error_message)
+
+    course, course_created = Course.objects.get_or_create(year__iexact=year, term__iexact=term, code__iexact=code)
+
+    if course_created:
+        logger.debug("new course")
+
+    course.year = year
+    course.term = term
+    course.code = code
+    course.subject = subject
+    course.numberOfStudents = students
+    course.has_course = 'c' in types
+    course.has_exercises = 'ex' in types
+    course.has_project = 'proj' in types
+    course.has_practical_work = 'tp' in types
+    course.taughtInGerman = 'allemand' in languages
+    course.taughtInEnglish = 'anglais' in languages
+    course.taughtInFrench = 'français' in languages
+
+    course.save()
 
     # time to deal with the teachings
     for teacher in teachers:
-        try:
-            teaching = Teaching.objects.get(person=teacher, course=course)
-        except ObjectDoesNotExist:
-            teaching = Teaching()
+        teaching, teaching_created = Teaching.objects.get_or_create(person=teacher, course=course)
+        if teaching_created:
             teaching.person = teacher
             teaching.course = course
             teaching.save()
 
 
 def load_mappings():
+    logger.info("loading mappings file")
+
     filename = settings.FIRST_NAME_LAST_NAME_MAPPING
     names = list()
     import io
@@ -173,6 +228,7 @@ def load_mappings():
 
 
 def check_groups_are_present():
+    logger.info("checking base groups (teachers, phds) ar existing")
     try:
         teachers = Group.objects.get(name="teachers")
     except ObjectDoesNotExist:
