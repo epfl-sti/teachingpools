155d9caa593c4824aacbc6af5a0d52fdf7520d23
Emmanuel Jaep <emmanuel.jaep@epfl.ch>
2019-12-20 13:49:38 +0100

    moved local dev from sqlite3 to postgres in order to be able to work with restores from production.

--- teaching_pool_project/settings/local.py
+++ teaching_pool_project/settings/local.py
@@ -3,11 +3,14 @@ import os
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DJANGO_POSTGRES_DB_NAME', 'teaching_pool'),
+        'USER': os.environ.get('DJANGO_POSTGRES_USERNAME', 'postgres'),
+        'PASSWORD': os.environ.get('DJANGO_POSTGRES_PASSWORD', 'password'),
+        'HOST': os.environ.get('DJANGO_POSTGRES_HOSTNAME', 'localhost'),
+        'PORT': '',
     }
 }
 
