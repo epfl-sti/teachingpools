ef47aa8661fbe31ae1af7f6e0bf4f2b6e04a6f14
Emmanuel Jaep <emmanuel.jaep@epfl.ch>
2020-04-01 15:59:16 +0200

    format

--- teaching_pool_project/web/views/timereporting_views.py
+++ teaching_pool_project/web/views/timereporting_views.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from datetime import datetime, date
 import json
 import logging
 import re
+from datetime import date, datetime
 from functools import wraps
 
 import xlwt
@@ -13,9 +13,10 @@ from django.contrib import messages
 from django.contrib.auth import get_user_model, login
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.models import Group, User
-from django.core.exceptions import ObjectDoesNotExist, PermissionDenied
 from django.core import serializers
-from django.db.models import Prefetch, Q, F, ExpressionWrapper, IntegerField, Value
+from django.core.exceptions import ObjectDoesNotExist, PermissionDenied
+from django.db.models import (ExpressionWrapper, F, IntegerField, Prefetch, Q,
+                              Value)
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render
 from django.template import Context
@@ -41,6 +42,7 @@ def is_superuser():
         if u.is_superuser:
             return True
         raise PermissionDenied
+
     return user_passes_test(has_superuser_profile)
 
 
@@ -50,6 +52,7 @@ def is_staff():
         if u.is_staff:
             return True
         raise PermissionDenied
+
     return user_passes_test(has_staff_profile)
 
 
@@ -59,10 +62,11 @@ def is_staff_or_teacher():
             return True
         elif u.is_staff:
             return True
-        elif u.groups.filter(name='teachers').exists():
+        elif u.groups.filter(name="teachers").exists():
             return True
         else:
             raise PermissionDenied
+
     return user_passes_test(has_staff_or_teacher_profile)
 
 
@@ -70,63 +74,67 @@ def group_required(*group_names):
     """Requires user membership in at least one of the groups passed in."""
 
     def in_groups(u):
-        logger.debug("Checking if %s is member of the '%s' group(s)",
-                     u.username, group_names)
+        logger.debug(
+            "Checking if %s is member of the '%s' group(s)", u.username, group_names
+        )
         if u.is_authenticated:
             if bool(u.groups.filter(name__in=group_names)) | u.is_superuser:
                 return True
         raise PermissionDenied
+
     return user_passes_test(in_groups)
 
 
-@group_required('phds')
+@group_required("phds")
 def get_user_time_reports(request):
     context = {}
-    return render(request, 'web/timereporting/list.html', context)
+    return render(request, "web/timereporting/list.html", context)
 
 
 @is_staff_or_teacher()
 def get_time_reports(request, year, term):
-    context = {
-        'year': year,
-        'term': term
-    }
-    return render(request, 'web/timereporting/list_all.html', context)
+    context = {"year": year, "term": term}
+    return render(request, "web/timereporting/list_all.html", context)
 
 
-@group_required('phds')
+@group_required("phds")
 def add_time_report(request):
-    time_reporting_is_open = config.get_config('time_reporting_is_open')
+    time_reporting_is_open = config.get_config("time_reporting_is_open")
     if time_reporting_is_open == False:
-        messages.error(request, "You are not allowed to enter new time reporting entries at the moment.")
+        messages.error(
+            request,
+            "You are not allowed to enter new time reporting entries at the moment.",
+        )
         raise PermissionDenied
 
     timereporting = TimeReport(created_by=request.user)
-    timereporting_form = TimeReportForm(request.POST or None, instance=timereporting, user=request.user)
+    timereporting_form = TimeReportForm(
+        request.POST or None, instance=timereporting, user=request.user
+    )
 
-    if request.method == 'POST':
+    if request.method == "POST":
         if timereporting_form.is_valid():
             timereporting.save()
             messages.success(request, "Time reporting entry successfully saved.")
-            return redirect('web:get_time_reports')
+            return redirect("web:get_time_reports")
         else:
             messages.error(request, "Please correct the errors below and resubmit.")
 
     context = {
-        'action': 'Add',
-        'form': timereporting_form,
+        "action": "Add",
+        "form": timereporting_form,
     }
 
-    return render(request, 'web/forms/timereporting/add.html', context)
+    return render(request, "web/forms/timereporting/add.html", context)
 
 
-@group_required('phds')
+@group_required("phds")
 def delete_time_report(request, id):
     try:
         time_report = TimeReport.objects.get(pk=id)
     except ObjectDoesNotExist:
         messages.error(request, "Unable to find the time report in the database")
-        return redirect('web:get_time_reports')
+        return redirect("web:get_time_reports")
 
     if time_report.created_by != request.user:
         messages.error(request, "You are not allowed to delete this entry")
@@ -134,208 +142,255 @@ def delete_time_report(request, id):
     else:
         time_report.delete()
         messages.success(request, "Entry successfully deleted")
-        return redirect('web:get_time_reports')
+        return redirect("web:get_time_reports")
 
 
-@group_required('phds')
+@group_required("phds")
 def edit_time_report(request, id):
     try:
         time_report = TimeReport.objects.get(pk=id)
     except ObjectDoesNotExist:
         messages.error(request, "Unable to find the time report in the database")
-        return redirect('web:time_reports')
+        return redirect("web:time_reports")
 
     if time_report.created_by != request.user:
         messages.error(request, "You are not allowed to edit this entry")
         raise PermissonDenied
 
-    time_report_form = TimeReportForm(request.POST or None, instance=time_report, user=request.user)
+    time_report_form = TimeReportForm(
+        request.POST or None, instance=time_report, user=request.user
+    )
 
-    if request.method == 'POST':
+    if request.method == "POST":
         if time_report_form.is_valid():
             time_report.save()
             messages.success(request, "Time reporting entry successfully edited.")
-            return redirect('web:get_time_reports')
+            return redirect("web:get_time_reports")
         else:
             messages.error(request, "PLease correct the errors below and resubmit.")
 
     context = {
-        'action': 'Edit',
-        'form': time_report_form,
+        "action": "Edit",
+        "form": time_report_form,
     }
 
-    return render(request, 'web/forms/timereporting/add.html', context)
+    return render(request, "web/forms/timereporting/add.html", context)
 
 
-@group_required('phds')
+@group_required("phds")
 def get_user_time_reports_api(request):
     if request.is_ajax():
         entries = TimeReport.objects.filter(created_by=request.user).all()
-        return_value = serializers.serialize('json', entries)
+        return_value = serializers.serialize("json", entries)
     else:
-        return_value = 'fail'
+        return_value = "fail"
 
     mimetype = "application/json"
     return HttpResponse(return_value, mimetype)
+
 
 def default(o):
     if isinstance(o, (datetime, date)):
         return o.isoformat()
 
+
 @is_staff_or_teacher()
 def get_time_reports_api(request, year, term):
     if request.is_ajax():
-        qs = TimeReport.objects \
-                .filter(year=year, term=term) \
-                .annotate(
-                    created_by_first_name=F('created_by__first_name'),
-                    created_by_last_name=F('created_by__last_name'),
-                    ) \
-                .all().values()
+        qs = (
+            TimeReport.objects.filter(year=year, term=term)
+            .annotate(
+                created_by_first_name=F("created_by__first_name"),
+                created_by_last_name=F("created_by__last_name"),
+            )
+            .all()
+            .values()
+        )
         for item in qs:
             total_hours = 0
-            if item['master_thesis_supervision_hours']:
-                total_hours += item['master_thesis_supervision_hours']
-            if item['class_teaching_exam_hours']:
-                total_hours += item['class_teaching_exam_hours']
-            if item['class_teaching_practical_work_hours']:
-                total_hours += item['class_teaching_practical_work_hours']
-            if item['class_teaching_preparation_hours']:
-                total_hours += item['class_teaching_preparation_hours']
-            if item['class_teaching_teaching_hours']:
-                total_hours += item['class_teaching_teaching_hours']
-            if item['semester_project_supervision_hours']:
-                total_hours += item['semester_project_supervision_hours']
-            if item['other_job_hours']:
-                total_hours += item['other_job_hours']
-            if item['MAN_hours']:
-                total_hours += item['MAN_hours']
-            if item['exam_proctoring_and_grading_hours']:
-                total_hours += item['exam_proctoring_and_grading_hours']
-            item['total_hours'] = total_hours
+            if item["master_thesis_supervision_hours"]:
+                total_hours += item["master_thesis_supervision_hours"]
+            if item["class_teaching_exam_hours"]:
+                total_hours += item["class_teaching_exam_hours"]
+            if item["class_teaching_practical_work_hours"]:
+                total_hours += item["class_teaching_practical_work_hours"]
+            if item["class_teaching_preparation_hours"]:
+                total_hours += item["class_teaching_preparation_hours"]
+            if item["class_teaching_teaching_hours"]:
+                total_hours += item["class_teaching_teaching_hours"]
+            if item["semester_project_supervision_hours"]:
+                total_hours += item["semester_project_supervision_hours"]
+            if item["other_job_hours"]:
+                total_hours += item["other_job_hours"]
+            if item["MAN_hours"]:
+                total_hours += item["MAN_hours"]
+            if item["exam_proctoring_and_grading_hours"]:
+                total_hours += item["exam_proctoring_and_grading_hours"]
+            item["total_hours"] = total_hours
     else:
-        return_value = 'fail'
+        return_value = "fail"
 
     mimetype = "application/json"
     return HttpResponse(json.dumps(list(qs), default=default), mimetype)
 
 
-@group_required('phds')
+@group_required("phds")
 def autocomplete_my_courses(request):
     if request.is_ajax():
-        q = request.GET.get('term', '')
+        q = request.GET.get("term", "")
 
         # get all the courses for which the user has been hired
-        applications = Applications.objects.filter(applicant=request.user, status="Hired").select_related('course').all()
+        applications = (
+            Applications.objects.filter(applicant=request.user, status="Hired")
+            .select_related("course")
+            .all()
+        )
         courses_ids = []
         [courses_ids.append(application.course.id) for application in applications]
-        courses = Course.objects.filter(Q(code__icontains=q) | Q(subject__icontains=q), id__in=courses_ids).all()
+        courses = Course.objects.filter(
+            Q(code__icontains=q) | Q(subject__icontains=q), id__in=courses_ids
+        ).all()
 
         return_value = list()
-        [return_value.append("{} ({} / {} / {})".format(course.subject, course.year, course.term, course.code)) for course in courses]
+        [
+            return_value.append(
+                "{} ({} / {} / {})".format(
+                    course.subject, course.year, course.term, course.code
+                )
+            )
+            for course in courses
+        ]
         return_value = json.dumps(return_value)
     else:
-        return_value = 'fail'
+        return_value = "fail"
 
-    mimetype = 'application/json'
+    mimetype = "application/json"
     return HttpResponse(return_value, mimetype)
 
 
-@group_required('phds')
+@group_required("phds")
 def autocomplete_all_teachers(request):
     if request.is_ajax:
-        q = request.GET.get('term', '')
+        q = request.GET.get("term", "")
         teachers = Group.objects.get(name="teachers").user_set.all()
         persons_ids = []
         [persons_ids.append(person.id) for person in teachers]
-        persons = Person.objects.filter(Q(last_name__icontains=q) | Q(first_name__icontains=q), id__in=persons_ids)
+        persons = Person.objects.filter(
+            Q(last_name__icontains=q) | Q(first_name__icontains=q), id__in=persons_ids
+        )
         return_value = list()
-        [return_value.append("{}, {} ({})".format(person.last_name, person.first_name, person.sciper)) for person in persons]
+        [
+            return_value.append(
+                "{}, {} ({})".format(person.last_name, person.first_name, person.sciper)
+            )
+            for person in persons
+        ]
         return_value = json.dumps(return_value)
     else:
-        return_value = 'fail'
-
-    mimetype = 'application/json'
-    return HttpResponse(return_value, mimetype)
-
-
-@group_required('phds')
-def autocomplete_all_students(request):
-    if request.is_ajax:
-        q = request.GET.get('term', '')
-        students = epfl_ldap.get_students_by_partial_first_name_or_partial_last_name(settings, q, q)
-        return_value = list()
-        [return_value.append("{}, {} ({})".format(student['last_name'], student['first_name'], student['sciper'])) for student in students]
-        return_value = json.dumps(return_value)
-    else:
-        return_value = 'fail'
+        return_value = "fail"
 
     mimetype = "application/json"
     return HttpResponse(return_value, mimetype)
 
 
-@group_required('phds')
+@group_required("phds")
+def autocomplete_all_students(request):
+    if request.is_ajax:
+        q = request.GET.get("term", "")
+        students = epfl_ldap.get_students_by_partial_first_name_or_partial_last_name(
+            settings, q, q
+        )
+        return_value = list()
+        [
+            return_value.append(
+                "{}, {} ({})".format(
+                    student["last_name"], student["first_name"], student["sciper"]
+                )
+            )
+            for student in students
+        ]
+        return_value = json.dumps(return_value)
+    else:
+        return_value = "fail"
+
+    mimetype = "application/json"
+    return HttpResponse(return_value, mimetype)
+
+
+@group_required("phds")
 def autocomplete_all_units(request):
     if request.is_ajax:
-        q = request.GET.get('term', '')
+        q = request.GET.get("term", "")
         units = epfl_ldap.get_units_by_partial_name_or_partial_last_acronym(settings, q)
         return_value = json.dumps(units)
     else:
-        return_value = 'fail'
+        return_value = "fail"
 
     mimetype = "application/json"
     return HttpResponse(return_value, mimetype)
 
 
-@group_required('phds')
+@group_required("phds")
 def get_teacher(request, id):
     if request.is_ajax():
         person = get_object_or_404(Person, pk=id)
-        if not person.groups.filter(name='teachers').exists():
-            return_value = ''
+        if not person.groups.filter(name="teachers").exists():
+            return_value = ""
         else:
-            return_value = serializers.serialize('json', [person], fields=['first_name', 'last_name'])
+            return_value = serializers.serialize(
+                "json", [person], fields=["first_name", "last_name"]
+            )
     else:
-        return_value = 'fail'
+        return_value = "fail"
 
-    mimetype = 'application/json'
+    mimetype = "application/json"
     return HttpResponse(return_value, mimetype)
 
 
-@group_required('phds')
+@group_required("phds")
 def get_course(request, id):
     if request.is_ajax():
         course = get_object_or_404(Course, pk=id)
-        return_value = serializers.serialize('json', [course], fields=['year', 'term', 'code', 'subject'])
+        return_value = serializers.serialize(
+            "json", [course], fields=["year", "term", "code", "subject"]
+        )
     else:
-        return_value = 'fail'
+        return_value = "fail"
 
-    mimetype = 'application/json'
+    mimetype = "application/json"
     return HttpResponse(return_value, mimetype)
 
 
 @is_staff_or_teacher()
 def reports_entry_page(request):
-    return render(request, 'web/timereporting/reports_entry_page.html')
+    return render(request, "web/timereporting/reports_entry_page.html")
 
 
 @is_staff_or_teacher()
 def get_reports_years(request):
     if request.is_ajax():
-        years = list(TimeReport.objects.values_list('year', flat=True).distinct().order_by('year'))
+        years = list(
+            TimeReport.objects.values_list("year", flat=True)
+            .distinct()
+            .order_by("year")
+        )
     else:
-        return_value = 'fail'
+        return_value = "fail"
 
-    mimetype = 'application/json'
+    mimetype = "application/json"
     return HttpResponse(json.dumps(years), mimetype)
 
 
 @is_staff_or_teacher()
 def get_reports_terms(request):
     if request.is_ajax():
-        terms = list(TimeReport.objects.values_list('term', flat=True).distinct().order_by('term'))
+        terms = list(
+            TimeReport.objects.values_list("term", flat=True)
+            .distinct()
+            .order_by("term")
+        )
     else:
-        return_value = 'fail'
+        return_value = "fail"
 
-    mimetype = 'application/json'
+    mimetype = "application/json"
     return HttpResponse(json.dumps(terms), mimetype)
